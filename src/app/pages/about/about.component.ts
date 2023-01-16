8efd5f722291071f5206e153afb16269ece99f72
Salman <salman250195@gmail.com>
2023-01-16 15:57:39 +0530

    responsive made

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -24,5 +24,15 @@ export class AboutComponent implements OnInit {
         "../../assets/img/cyber_security/cyber_security3.jpg"
       ];
 
+      arrayOfImgUrls = [
+        '../../assets/img/cyber_security/cyber_security.jpg',
+        '../../assets/img/cyber_security/cyber_security1.jpg',
+        '../../assets/img/cyber_security/cyber_security2.jpg',
+        '../../assets/img/cyber_security/cyber_security3.jpg',
+        // 'https://images.unsplash.com/photo-1545893835-abaa50cbe628?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2073&q=80',
+        // 'https://images.unsplash.com/photo-1546884680-a1de22e94d50?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        
+      ]
+
 
 }
